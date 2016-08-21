27cd2d838cf7d94dc0781aa2c5f455f71a141b45
Thomas Eldblom <the@sitecore.net>
2016-08-21 22:33:24 +1000

    Initial commit

---
+++ gulp-config.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+  var instanceRoot = "C:\\websites\\Habitat.dev.local";
+  var config = {
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
+    solutionName: "Sitecore.Demo.Group",
+    buildConfiguration: "Debug",
+    runCleanBuilds: false
+  };
+  return config;
+}
