0fb149ff3d73928c0ae0471e32f6daf69ec75d0f
sumith <sumithpd@gmail.com>
2017-06-25 00:23:25 +0100

    Fixed the path to project folder

--- gulp-config.js
+++ gulp-config.js
@@ -1,12 +1,12 @@
 module.exports = function() {
-  var instanceRoot = "C:\\Code\\Sitecore\\Sites\\Habitat.dev.local";
-  var config = {
-    websiteRoot: instanceRoot + "\\Website",
-    sitecoreLibraries: instanceRoot + "\\Website\\bin",
-    licensePath: instanceRoot + "\\Data\\license.xml",
-    solutionName: "Sitecore.Demo.Group",
-    buildConfiguration: "Debug",
-    runCleanBuilds: false
-  };
-  return config;
+    var instanceRoot = "C:\\websites\\Habitat.dev.local";
+    var config = {
+        websiteRoot: instanceRoot + "\\Website",
+        sitecoreLibraries: instanceRoot + "\\Website\\bin",
+        licensePath: instanceRoot + "\\Data\\license.xml",
+        solutionName: "Sitecore.Demo.Group",
+        buildConfiguration: "Debug",
+        runCleanBuilds: false
+    };
+    return config;
 }
